2700bd0fe1c0a2f54c32af3bdcc4228f8f49558d
alex.chang <alex.chang@sensestarl.live>
2021-11-03 20:31:31 +0800

    test flatten.

--- values.py
+++ values.py
@@ -1,30 +1,42 @@
+import collections
+
 def read(file):
     with open(file,'r',encoding='utf-8') as f:
         data = f.readlines()
     return data
-v = read('/Users/jason/Documents/sensestar/sw-infra/helm/sw-api/values-test.yaml') # this row is on mac env.
+z = read('/Users/jason/Documents/sensestar/sw-infra/helm/sw-api/values-test.yaml') # this row is on mac env.
 # v = read('C:\\Users\\Peter\\Documents\\Company\\sensestar\\sw-infra\\helm\\sw-api\\values-test.yaml') # this row is windows env.
-t1 = []
+# t1 = []
 
 # clane values row data
-for x in v:
-    t1.append(x.replace('\n', '').replace('-', '').replace(',', '') )
+# for x in v:
+#     t1.append(x.replace('\n', '').replace('-', '').replace(',', '').split(':'))
 
+def flatten(d, parent_key='', sep='.'):
+    items = []
+    for k, v in d.items():
+        new_key = parent_key + sep + k if parent_key else k
+        if isinstance(v, collections.MutableMapping):
+            items.extend(flatten(v, new_key, sep=sep).items())
+        else:
+            items.append((new_key, v))
+    return dict(items)
+print(dict(z))
 # get list len
-listcount = len(t1)
+# listcount = len(t1)
 
 # write file
-path = '/Users/jason/Documents/test-deploy/value-logs.txt' # this row is on mac env.
+# path = '/Users/jason/Documents/test-deploy/value-logs.txt' # this row is on mac env.
 # path = 'C:\\Users\\Peter\\Documents\\Company\\test-deploy\\value-logs.txt' # this row is windows env.
-f = open(path,'w',encoding='utf-8')
+# f = open(path,'w',encoding='utf-8')
 
-for i in range(listcount):
-    try:
-        if len(t1[i])>0:
-            print((t1[i]),file=f)
-        else:
-            pass
-    except IndexError:
-        continue
+# for i in range(listcount):
+#     try:
+#         if len(t1[i])>0:
+#             print((t1[i]),file=f)
+#         else:
+#             pass
+#     except IndexError:
+#         continue
 
-f.close()
+# f.close()

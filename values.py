3edbfca4f3b4a3501e39d7c72371eed2d333c1e9
alex.chang <alex.chang@sensestarl.live>
2021-11-04 18:29:04 +0800

    add desciption in values.py

--- values.py
+++ values.py
@@ -4,15 +4,21 @@ import pandas as pd
 
 # d = {'a':1,'c':{'a':2,'b':{'x':5,'y':10}},'d':[1,2,3]}
 
+# log path
 log_path = '/Users/jason/Documents/test-deploy/values_log.txt'
 f = open(log_path,'w',encoding='utf-8')
 
+# read yaml file func
 def read_yaml(file):
     with open(file,'r',encoding='utf8') as f:
         data = yaml.safe_load(f)
     return data
+
+# import data
 a = read_yaml('/Users/jason/Documents/sensestar/sw-infra/helm/sw-api/values-test.yaml')
 # a = read_yaml('/Users/jason/Documents/test-deploy/123.yaml')
+
+# flatten data func
 def flatten_dict(a: MutableMapping, sep: str= '_') -> MutableMapping:
     [flat_dict] = pd.json_normalize(a, sep=sep).to_dict(orient='records')
     return flat_dict
